a4a262f42d95fd88791e79bb0003de3dc5275f7c
BekAs <bekas.omsk@mail.ru>
2024-01-24 23:40:48 +0600

    Добавлено решение задачи

--- newArray.cs
+++ newArray.cs
@@ -1,2 +1,34 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿/*
+Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+*/
+Console.Clear();
+string[] firstArray = new string[5] { "honda", "kia", "bmw", "audi", "ford" };
+string[] secondArray = new string[firstArray.Length];
+
+void fullSecondArray(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            secondArray[count] = firstArray[i];
+            count++;
+        }
+    }
+}
+
+void printArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+fullSecondArray(firstArray, secondArray);
+printArray(secondArray);
